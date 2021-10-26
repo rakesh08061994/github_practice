50ad3688152d471579634696a2e21dd9856d09c2
rakesh08061994 <jangidrakesh71@gmail.com>
2021-10-26 15:39:33 +0530

    initial commit

---
+++ Project (1).py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+Style.DIM+"Hi My name is Aman Kharwal "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+Style.BRIGHT+"Hi My name is Aman Kharwal")
+print(Fore.RED + Back.GREEN+Style.NORMAL+"Hi My name is Aman Kharwal")
